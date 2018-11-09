f5ea8373693ab77da866f8f604b766602756a61c
Hoi Yuen <yh0016@hotmail.com>
2018-11-08 23:43:14 -0500

    feature-#1 moved app script to components folder

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../app.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        return (
+            <div className="app">
+
+            </div>
+        )
+    }
+}
+
+export default App
