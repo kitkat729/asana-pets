66c9c5443d9b454ed588594795ebf133166dd36b
Hoi Yuen <yh0016@hotmail.com>
2018-11-13 02:17:36 -0500

    feature-#5 add srcset and sizes based on breakpoints

--- src/components/MasonryCard.js
+++ src/components/MasonryCard.js
@@ -2,12 +2,24 @@ import React from 'react'
 
 const MasonryCard = (props) => {
     const {item} = props
+    const srcSet = [
+        item.photo.urls.thumb + " 200w",
+        item.photo.urls.small + " 400w",
+        item.photo.urls.regular + " 1080w",
+    ]
+    const sizes = [
+        "(min-width: 1200px) 355px",
+        "(min-width: 768px) 215px",
+        "100vw",
+    ]
 
     return (
         <li className="card">
             <img
+                srcSet={srcSet.join(',')}
+                sizes={sizes.join(',')}
                 className="card-img-top img-fluid"
-                src={item.photo.urls.thumb}
+                src={item.photo.urls.regular}
                 alt={typeof item.photo.description !==  undefined ? item.photo.description : ''}
             />
         </li>
